1df7df731ebdcf4a3c83a2d45648bed26653d2bb
Durgeshnunna9 <durgesh.nunna.dev@gmail.com>
2025-07-13 09:31:47 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,7 @@
    import { getFirestore } from "firebase/firestore";
 
    const firebaseConfig = {
-    apiKey: "AIzaSyBLhkhcd4dBvmzQfgdP7Ymz5xXxv_ejA9s",
-    authDomain: "sansan-group.firebaseapp.com",
-    projectId: "sansan-group",
-    storageBucket: "sansan-group.firebasestorage.app",
-    messagingSenderId: "1058311975076",
-    appId: "1:1058311975076:web:61cd17452f538c0ca87631",
-    measurementId: "G-N16GP5Y3V3"
+    
    };
 
    const app = initializeApp(firebaseConfig);
